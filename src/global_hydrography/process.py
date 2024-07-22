fad01044b58eb68abfe00dbddbe03d3d5a1b523b
Anthony Aufdenkampe <aaufdenkampe@limno.com>
2024-07-22 17:13:58 -0500

    Completed full processing pipeline!
    @ptomasula & @kieranbartels, the final helper function that pulls it all together is the `rocess_tdx_streams_basins()` funciton at the end of `examples/4_ProcessBasinToParquet.ipynb`

--- src/global_hydrography/process.py
+++ src/global_hydrography/process.py
@@ -5,6 +5,7 @@ import geopandas as gpd
 import pandas as pd
 
 from global_hydrography.preprocess import TDXPreprocessor
+from global_hydrography.delineation.mnsi import MNSI_FIELDS
 
 
 
@@ -12,7 +13,7 @@ def select_tdx_files(
     directory_path: Path,
     tdxhydroregion: int,
     file_extension: str,
-)-> list[Path]:
+)-> tuple[Path]:
     """Select pairs of TDX 'streamnet' and 'streamreach_basins'
     files for processing together.
 
@@ -27,9 +28,45 @@ def select_tdx_files(
             str(tdxhydroregion) in item.name
         ):
             if 'streamnet' in item.name:
-                streamnet_file = item
+                streamnet_filepath = item
             if 'basins' in item.name:
-                basins_file = item
+                basins_filepath = item
 
-    return (streamnet_file, basins_file)
+    return (streamnet_filepath, basins_filepath)
+
+
+def create_basins_mnsi(
+    basins_gdf: gpd.GeoDataFrame,
+    streams_mnsi_gdf: gpd.GeoDataFrame,
+)-> tuple[gpd.GeoDataFrame]:
+    """Create Basins GeoDataFrame with MNSI fields from streamnet_mnsi_gdf.
+
+    Parameters:
+        basins_gdf: TDX Streamreach Basins dataset, with 'streamID' renamed to 'LINKNO'.
+        streams_mnsi_gdf: TDX Stream Network dataset with MNSI fields added.
+
+    Return: A tuple of GeoDataFrames
+        basins_mnsi_gdf: A copy of the basins_gdf appended with three MNSI fields.
+        streams_no_basin_gdf: A gdf of the streamnet LINKs that have no associated basins.
+    """
+    
+    # Perform a right join, for all rows in streamnet,
+    # potentially creating some rows with no basin geometries
+    basins_mnsi_gdf = pd.merge(
+        basins_gdf, 
+        streams_mnsi_gdf[MNSI_FIELDS], 
+        how='right', 
+        on='LINKNO',
+    )
+    # Save streamnet rows that don't have a basin geometry
+    streams_no_basin_gdf = streams_mnsi_gdf[basins_mnsi_gdf.geometry==None].copy(deep=True)
+
+    # Drop no-geometry rows from basins gdf
+    basins_mnsi_gdf.drop(
+        streams_no_basin_gdf.index.to_list(), 
+        inplace=True,
+    )
+    
+    return (basins_mnsi_gdf, streams_no_basin_gdf)
+
 
